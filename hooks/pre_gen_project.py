21c399cea25e4a4bd1e96cc20837344c4479974d
Molina Lautaro <MolinaLautaro@protonmail.com>
2023-04-07 12:08:55 -0300

    Ready to use

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,17 @@
+#pre_gen is an script that works pre generation of the cookiecutter excution
+import os
+import sys
+
+project_slug = "{{ cookiecutter.project_slug }}"
+
+ERROR_COLOR = "\x1b[31m"
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+if project_slug.startswith("x"):
+   print(f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template.{RESET_ALL}")
+
+   sys.exit(1)
+
+print(f"{MESSAGE_COLOR}Let's do it! You're are going to create something awesome!")
+print(f"Creating project at { os.getcwd() }{RESET_ALL}")
---
+++ {{ cookiecutter.project_slug }}/README.md
@@ -0,0 +1,5 @@
+# {{ cookiecutter.project_title}}
+
+##  {{ cookiecutter.project_description }}
+
+By: {{ cookiecutter.project_author_name}}
